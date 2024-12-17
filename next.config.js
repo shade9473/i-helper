5c7ce7436e849925852fd5ebc124a0546104fae7
shade9473 <shade9473@gmail.com>
2024-12-17 11:03:28 -0800

    fix: Update Next.js and Cloudflare configs for static export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: '',
+  assetPrefix: '',
 }
 
 module.exports = nextConfig
